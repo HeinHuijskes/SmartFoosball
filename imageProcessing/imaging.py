4778e6d69071f1a3f46c0f316e155c93c0c3fa94
Iristk <iristenklooster123@gmail.com>
2024-09-16 14:12:59 +0200

    Camera feed shows correctly

--- imageProcessing/imaging.py
+++ imageProcessing/imaging.py
@@ -2,20 +2,23 @@ import cv2
 
 camera = None
 
-
 def init_camera():
     global camera
     if not camera:
+        # camera = cv2.VideoCapture(0)  # Use just 0 for default camera
         camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-        # camera.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 640)
-        # camera.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
+        camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
         print("Camera initialized successfully.")
 
         while True:
             ret, frame = camera.read()
             if ret:
-                cv2.imshow(frame)
+                cv2.imshow("video", frame)
 
+            # Wait for 1ms and check if the user pressed 'q' to exit
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
 if __name__ == '__main__':
     try:
@@ -24,6 +27,3 @@ if __name__ == '__main__':
         if camera:
             camera.release()
             cv2.destroyAllWindows()
-
-
-
