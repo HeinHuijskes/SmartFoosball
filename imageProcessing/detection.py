8f20b021eea768c78ee10202aff2e1f86d6fd874
Hein <h.h.l.huijskes@student.utwente.nl>
2024-09-18 16:25:42 +0200

    Angle works almost always perfectly

--- imageProcessing/detection.py
+++ imageProcessing/detection.py
@@ -10,15 +10,16 @@ detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
 
 class Detection():
     def __init__(self):
-        self.foos_men_min = 250
+        self.foos_men_min = 150
         self.foos_men_max = 1500
         self.corners = [[], [], [], []]
         self.frames = 0
+        self.rotate_matrix = []
 
     def run(self, frame, mode = MODE['NORMAL']):
 
-        frame = self.scale(frame, 0.5)
         frame = self.aruco(frame)
+        frame = self.scale(frame, 0.5)
 
         frame = self.applyMode(mode, frame)
         return self.foosMenDetection(frame)
@@ -37,7 +38,7 @@ class Detection():
 
         return cv2.bitwise_and(frame, frame, mask = mask)
 
-    def aruco(self, frame, calibration_time = 30):
+    def aruco(self, frame, calibration_time = 5):
         '''Detects aruco, returns the bounding box of the foosball table'''
 
         frame = cv2.flip(frame, 1)
@@ -45,8 +46,11 @@ class Detection():
             height, width, _ = frame.shape
             self.min_x = 0
             self.max_x = width
+            self.mid_x = math.ceil(width * 0.5)
             self.min_y = 0
             self.max_y = height
+            self.mid_y = math.ceil(height * 0.5)
+            self.angle = 0
 
         elif self.frames < calibration_time: # TODO: Make in line with measured FPS
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -54,34 +58,47 @@ class Detection():
             cv2.convertScaleAbs(inverted, inverted, 3)
             (corners, ids, rejected) = detector.detectMarkers(inverted)
             ids = ids.flatten()
-            cv2.aruco.drawDetectedMarkers(frame, corners, ids)
+            # cv2.aruco.drawDetectedMarkers(frame, corners, ids)
 
             for (marker, id) in zip(corners, ids): 
                 corner = marker.reshape((4, 2))
-                (tl, tr, br, bl) = corner
-                cx = (tl[0] + br[0]) / 2
-                cy = (tl[1] + br[1]) / 2
+                maximum = 0
+                for (x, y) in corner:
+                    distance = math.sqrt((x - self.mid_x)**2 + (y - self.mid_y)**2)
+                    if distance > maximum:
+                        maximum = distance
+                        cx = x
+                        cy = y
                 if len(corners[0][0]) == 4:
                     self.corners[id].append((cx, cy))
-        
+
+        elif min([len(corner) for corner in self.corners]) < 1 and self.frames == calibration_time:
+            self.frames = 1
+            self.corners = [[], [], [], []]
+
+        elif self.frames > calibration_time*20:
+            self.frames = 1
+            self.corners = [[], [], [], []]
+
         elif self.frames == calibration_time:
             result = []
             for corner in self.corners:
                 average_x = sum([x for (x,y) in corner]) / len(corner)
                 average_y = sum([y for (x,y) in corner]) / len(corner)
                 result.append((average_x.item(), average_y.item()))
+            detected_corners = min([len(corner) for corner in self.corners])
             self.corners = result
-            print(f'Corner confidence: {len(corner)*10000//100/calibration_time}% ({len(corner)} corners detected in {calibration_time} frames)')
-            self.min_x = int(self.corners[1][0])
-            self.max_x = int(self.corners[2][0])
-            self.min_y = int(self.corners[1][1])
-            self.max_y = int(self.corners[2][1])
-            print(frame.shape)
-            print(self.corners)
-        
-        elif self.frames > 1000:
-            self.frames = 1
+            print(f'Corner confidence: {detected_corners*10000//100/(calibration_time-1)}% ({detected_corners} corners detected in {calibration_time-1} frames)')
+            self.min_x = int(min(self.corners[1][0], self.corners[2][0]))
+            self.max_x = int(max(self.corners[3][0], self.corners[0][0]))
+            self.min_y = int(min(self.corners[2][1], self.corners[3][1]))
+            self.max_y = int(max(self.corners[1][1], self.corners[0][1]))
 
+            self.angle = -int(math.degrees(math.atan(abs(self.corners[2][1]-self.corners[3][1])/abs(self.corners[2][0]-self.corners[3][0]))))
+            self.rotate_matrix = cv2.getRotationMatrix2D((self.mid_x, self.mid_y), self.angle, 1)
+        
+        if len(self.rotate_matrix) > 0:
+            frame = cv2.warpAffine(frame, self.rotate_matrix, frame.shape[1::-1], flags=cv2.INTER_LINEAR)
         frame = frame[max(self.min_y,0):self.max_y, self.min_x:max(self.max_x,0)]
         self.frames += 1
 
@@ -138,7 +155,7 @@ class Detection():
 
 def testDetect():
     detection = Detection()
-    video = cv2.VideoCapture('data/video/latest - Trim.mp4')
+    video = cv2.VideoCapture('data/video/shakiestcam.mp4')
     mode = 'FULL'
 
     nextFrame = True
