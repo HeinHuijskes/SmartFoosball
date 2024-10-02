527e7eaee593676ed214ce9293fd5e5f9dfa52b1
sophieT03 <ut.s2535068@gmail.com>
2024-10-02 11:47:33 +0200

    can update score on site with pressing 'l''or 'r '

--- hardware/aduino.py
+++ hardware/aduino.py
@@ -11,10 +11,18 @@ class Arduino:
 
     def run(self):
         while True:
-            time.sleep(5)
-            print("added goal")
-            self.game.add_goal(True)
-            time.sleep(0.1)
+            if keyboard.read_key() == "l":
+                print("pressed l")
+                self.game.add_goal(True)
+                time.sleep(0.1)
+            if keyboard.read_key() == "r":
+                print("pressed r")
+                self.game.add_goal(False)
+                time.sleep(0.1)
+            # time.sleep(5)
+            # print("added goal")
+            # self.game.add_goal(True)
+            # time.sleep(0.1)
 
     def key_press(self):
         if keyboard.read_key() == "s" :
