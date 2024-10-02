298a6aa59d1fbdee8ef322653ebabc328c489f22
Iristk <iristenklooster123@gmail.com>
2024-10-02 11:25:19 +0200

    decent goal detection

--- hardware/arduinoCode/arduinoCode.ino
+++ hardware/arduinoCode/arduinoCode.ino
@@ -4,22 +4,38 @@ int sensorPinBlue = 34; //define analog pin 2
 // int sensorPinRed = 35; //define analog pin 3
 int valueBlue = 0; 
 // int valueRed = 0;
+int counter = 0;
+int blueGoals = 0;
 
 
 
 
 
 void setup() {
-	Serial.begin(9600); 
+	Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);
   // analogWrite(LED_PIN, 20);
 
-} 
+}
 
 void loop() {
-	valueBlue = analogRead(sensorPinBlue); 
-	Serial.println(valueBlue, DEC); // light intensity
+	valueBlue = analogRead(sensorPinBlue);
+
+  if (valueBlue >= 950) {
+	  // Serial.println(valueBlue, DEC); // light intensity
+    // counter +=1;
+
+    Serial.print("Gooooooaaaalllll number ");
+    Serial.println(blueGoals);
+    blueGoals += 1;
+    counter = 0;
+    while(analogRead(sensorPinBlue) >= 950){
+
+    }
+
+  }
+
 
   // valueRed = analogRead(sensorPinRed); 
 	// Serial.println(valueRed, DEC); // light intensity
