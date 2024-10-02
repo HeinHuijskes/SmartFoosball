08e00718d364ddde18f128198d09fcc04d3ba019
Iristk <iristenklooster123@gmail.com>
2024-10-02 16:15:53 +0200

    update arduino code

--- hardware/arduinoCode/arduinoCode.ino
+++ hardware/arduinoCode/arduinoCode.ino
@@ -1,48 +1,24 @@
-#define LED_PIN 5
-
 int sensorPinBlue = 34; //define analog pin 2
 // int sensorPinRed = 35; //define analog pin 3
-int valueBlue = 0; 
+int valueBlue = 0;
 // int valueRed = 0;
 int counter = 0;
 int blueGoals = 0;
 
 
-
-
-
 void setup() {
 	Serial.begin(9600);
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, HIGH);
-  // analogWrite(LED_PIN, 20);
-
 }
 
 void loop() {
 	valueBlue = analogRead(sensorPinBlue);
 
   if (valueBlue >= 950) {
-	  // Serial.println(valueBlue, DEC); // light intensity
-    // counter +=1;
-
-    Serial.println("Goal red");
-//     Serial.println(blueGoals);
+    Serial.println("Goal blue");
     blueGoals += 1;
     counter = 0;
     while(analogRead(sensorPinBlue) >= 950){
 
     }
-
   }
-
-
-  // valueRed = analogRead(sensorPinRed); 
-	// Serial.println(valueRed, DEC); // light intensity
-  // value = 0;
-	// delay(100); 
-  // goal if value is higher than... for a length of...
-  // determine the team
-  // if goal, send call to endpoint laptop -> we need wifi on the esp
-  // later:update LEDs
 }
