57e76a95b5ef7e0e37d8ca12ae0e0618e23a4ab2
Iristk <iristenklooster123@gmail.com>
2024-10-03 10:30:36 +0200

    update arduino code to sophie's approach

--- hardware/arduino.py
+++ hardware/arduino.py
@@ -1,6 +1,58 @@
 from threading import Thread, Lock
 
 import serial
+
+
+
+# class Arduino:
+#     def __init__(self, com_port='COM3'):
+#         self.serialConnection = serial.Serial(com_port, 9600)
+#         self.goal = None
+#         self.reset = False
+#         self.thread = Thread(target=self.loop, daemon=True)
+#         self.lock = Lock()
+#         self.thread.start()
+#
+#     def loop(self):
+#         while True:
+#             if self.serialConnection.in_waiting:
+#                 self.lock.acquire()
+#
+#             line = self.get_line()
+#             if line == ('Goal red'):
+#                 self.goal = Team.RED
+#             elif line == ('Goal blue'):
+#                 self.goal = Team.BLUE
+#             elif line == "reset":
+#                 self.reset = True
+#
+#                 self.lock.release()
+#
+#     def get_goal_or_reset(self):
+#         self.lock.acquire()
+#         goal = self.goal
+#         self.goal = None
+#         reset = self.reset
+#         self.reset = False
+#         self.lock.release()
+#
+#         return goal, reset
+#
+#     def get_line(self):
+#         line = self.serialConnection.readline()
+#         line = line.decode('ascii').strip()
+#         return line
+#
+#
+# if __name__ == '__main__':
+#     pass
+
+import time
+
+import serial
+
+import keyboard
+
 from enum import Enum
 
 
@@ -10,44 +62,37 @@ class Team(Enum):
 
 
 class Arduino:
-    def __init__(self, com_port='COM3'):
-        self.serialConnection = serial.Serial(com_port, 9600)
-        self.goal = None
-        self.reset = False
-        self.thread = Thread(target=self.loop, daemon=True)
-        self.lock = Lock()
-        self.thread.start()
 
-    def loop(self):
+    def __init__(self, website, game, com_port='COM3'):
+        self.website = website
+        self.game = game
+        self.serialConnection = serial.Serial(com_port, 9600)
+        # self.lock = Lock()
+
+    def run(self):
         while True:
             if self.serialConnection.in_waiting:
-                self.lock.acquire()
+                line = self.get_line()
 
-            line = self.get_line()
-            if line == ('Goal red'):
-                self.goal = Team.RED
-            elif line == ('Goal blue'):
-                self.goal = Team.BLUE
-            elif line == "reset":
-                self.reset = True
+                # red is left
+                if line == 'Goal red':
+                    print("red goal")
+                    self.game.add_goal(True)
+                    time.sleep(0.1)
+                elif line == 'Goal blue':
+                    print("blue goal")
+                    self.game.add_goal(False)
+                    time.sleep(0.1)
+                elif line == "reset":
+                    pass
+    #             TODO add this funciton again
 
-                self.lock.release()
-
-    def get_goal_or_reset(self):
-        self.lock.acquire()
-        goal = self.goal
-        self.goal = None
-        reset = self.reset
-        self.reset = False
-        self.lock.release()
-
-        return goal, reset
+    def key_press(self):
+        if keyboard.read_key() == "s":
+            print("pressed s")
+            self.game.add_goal(True)
 
     def get_line(self):
         line = self.serialConnection.readline()
         line = line.decode('ascii').strip()
         return line
-
-
-if __name__ == '__main__':
-    pass
