76f369a72be173225c77b8b48f0b28c8259a190e
Mathijs-Vogelezang <70201164+Mathijs-Vogelezang@users.noreply.github.com>
2024-10-10 15:20:45 +0200

    arduino code works

--- hardware/arduinoCode/arduinoCode.ino
+++ hardware/arduinoCode/arduinoCode.ino
@@ -1,51 +1,72 @@
 #include <OneButton.h>
-#define LED_PIN 5
 #define BUTTON_PIN 26
 #define DELAY 2000
 
 int sensorPinBlue = 34; //define analog pin 2
-int valueBlue = 0;
+int sensorPinRed = 35; //define analog pin 3
+// int valueBlue = 0;
 int counterBlue = 0;
+int counterRed = 0;
 int blueGoals = 0;
-int treshold = 850;
+int tresholdBlue = 850;
+int tresholdRed = 700;
 OneButton resetButton;
 
 void setup() {
 	Serial.begin(9600);
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, HIGH);
   resetButton = OneButton(BUTTON_PIN, true, true);
-  resetButton.attachDuringLongPress(handleReset);
+  resetButton.attachPress(handleReset);
+
+  tresholdBlue = getTreshold(sensorPinBlue);
+  tresholdRed = getTreshold(sensorPinRed);
+
+  Serial.print("Blue treshold is: ");
+  Serial.println(tresholdBlue);
+  Serial.print("Red treshold is: ");
+  Serial.println(tresholdRed);
+
+
   Serial.println("start");
+
+
 }
 
 void loop() {
   resetButton.tick();
-	valueBlue = analogRead(sensorPinBlue);
-  // Serial.println(valueBlue);
+  checkGoal(sensorPinBlue, &counterBlue, "blue", tresholdBlue);
+  checkGoal(sensorPinRed, &counterRed, "red", tresholdRed);
 
-  if (valueBlue >= treshold) {
-    counterBlue++;
+  // Serial.println(analogRead(sensorPinRed));
+}
 
-    if (counterBlue >= 6){
-      Serial.println("Goal blue");
-      blueGoals++;
-      counterBlue = 0;
-      Serial.println(valueBlue);
+void checkGoal(int sensorPin, int *counter, String team, int treshold) {
+  int value = analogRead(sensorPin);
 
+  if (value >= treshold) {
+    (*counter)++;
+
+    if (*counter >= 6){
+      Serial.println("Goal " + team);
+      *counter = 0;
+      Serial.println(value);
       delay(DELAY);
-
-      while(valueBlue >= treshold){
-        // Serial.println(valueBlue);
-        delay(100);
-        valueBlue = analogRead(sensorPinBlue);
-
-      }
     }
   } else {
-    counterBlue = 0;
+    *counter = 0;
   }
 }
+
+int getTreshold(int sensorPin) {
+  int iterations = 10;
+  int total = 0;
+
+  for (int i = 0; i < iterations; i++) {
+    total += analogRead(sensorPin);
+  }
+
+  return (total/iterations) * 1.1;
+}
+
 
 static void handleReset() {
   Serial.println("reset");
